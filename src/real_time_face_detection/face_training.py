337860c19e19ae6ed67226fccadaf035d51db6e2
Michael Stuffer <michi_stuffer@outlook.de>
2023-02-11 01:08:57 +0100

    format changes for flake8 linting

--- src/real_time_face_detection/face_training.py
+++ src/real_time_face_detection/face_training.py
@@ -1,19 +1,21 @@
 ''''
 Training Multiple Faces stored on a DataBase:
-	==> Each face should have a unique numeric integer ID as 1, 2, 3, etc                       
-	==> LBPH computed model will be saved on trainer/ directory. (if it does not exist, pls create one)
-	==> for using PIL, install pillow library with "pip install pillow"
+    ==> Each face should have a unique numeric integer ID as 1, 2, 3, etc
+    ==> LBPH computed model will be saved on trainer/ directory. (if it does not exist, pls create one)
+    ==> for using PIL, install pillow library with "pip install pillow"
 
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition
 
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18   
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
 
 '''
 
+import os
+
 import cv2
 import numpy as np
-from PIL import Image #pillow package
-import os
+from PIL import Image  # pillow package
+
 import real_time_face_detection.utils as utils
 from real_time_face_detection.profiles import Profiles
 
@@ -23,17 +25,17 @@ def getImagesAndLabels(path, detector):
     # Set up a generator to iterate through the image directory
     image_dir = utils.image_dir_generator(path)
     # image_dir = [ os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-    faceSamples=[]
+    faceSamples = []
     ids = []
     # n_images = len(image_dir)
-    
+
     profiles = Profiles()
     static_id = 1
     blacklist = []
 
     for imagePath in image_dir:
-        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
-        img_numpy = np.array(PIL_img,'uint8')
+        pil_image = Image.open(imagePath).convert('L')    # convert it to grayscale
+        img_numpy = np.array(pil_image, 'uint8')
 
         name = os.path.split(imagePath)[-1].split(".")[1]
         if name in blacklist:
@@ -42,21 +44,21 @@ def getImagesAndLabels(path, detector):
             id = int(profiles.profiles[name])
             if id == 0:
                 id = profiles.profiles[name] = static_id
-                static_id += 1 
+                static_id += 1
         except KeyError:
             blacklist.append(name)
             print("picture face_id '" + name + "' is not registered in profile data base, skipping")
             continue
 
         faces = detector.detectMultiScale(img_numpy)
-        for (x,y,w,h) in faces:
-            faceSamples.append(img_numpy[y:y+h,x:x+w])
+        for (x, y, w, h) in faces:
+            faceSamples.append(img_numpy[y:y + h, x:x + w])
             ids.append(id)
 
         # utils.print_progress(i/n_images)
     profiles.flush()
 
-    return faceSamples,ids
+    return faceSamples, ids
 
 
 def main(args):
@@ -66,14 +68,12 @@ def main(args):
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-    print ("\n [INFO] Training face models")
-    faces,ids = getImagesAndLabels(path, detector)
+    print("\n [INFO] Training face models")
+    faces, ids = getImagesAndLabels(path, detector)
     recognizer.train(faces, np.array(ids))
 
     # Save the model into trainer/trainer.yml
     recognizer.write('model/trainer.yml')
-
-
 
     # Print the numer of faces trained and end program
     print("\n [INFO] Finished: {0} faces trained.".format(len(np.unique(ids))))
--- src/real_time_face_detection/real_time_recognition.py
+++ src/real_time_face_detection/real_time_recognition.py
@@ -1,60 +1,61 @@
 ''''
 Real Time Face Recogition
-	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc                       
-	==> LBPH computed model (trained faces) should be on trainer/ dir
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+    ==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc
+    ==> LBPH computed model (trained faces) should be on trainer/ dir
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition
 
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18  
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
 
 '''
 
-from configparser import ConfigParser
+from collections import deque as dq
+
 import cv2
 import numpy as np
-import os
-from real_time_face_detection.config_loader import ConfigLoader 
+
+from real_time_face_detection.config_loader import ConfigLoader
 from real_time_face_detection.profiles import Profiles
-import numpy as np
-from collections import deque as dq
+
 
 class FaceAveraging:
     def __init__(self, avg_frames: int) -> None:
-        self.N = avg_frames
+        self.n_frames = avg_frames
         self.faces = {}
-        self.kernel = np.ones(self.N)/self.N
+        self.kernel = np.ones(self.n_frames) / self.n_frames
 
-    def new_frame(self, x, y, w, h, id):
-        if id == "unknown":
+    def new_frame(self, x, y, w, h, face_id):
+        if face_id == "unknown":
             return (x, y, w, h)
-        if id not in self.faces:
-            self.faces[id] = (dq([x], self.N), dq([y], self.N), dq([w], self.N), dq([h], self.N))
+        if face_id not in self.faces:
+            self.faces[face_id] = (dq([x], self.n_frames), dq([y], self.n_frames),
+                                   dq([w], self.n_frames), dq([h], self.n_frames))
         else:
-            self.faces[id][0].append(x)
-            self.faces[id][1].append(y)
-            self.faces[id][2].append(w)
-            self.faces[id][3].append(h)
-        f = self.faces[id]
+            self.faces[face_id][0].append(x)
+            self.faces[face_id][1].append(y)
+            self.faces[face_id][2].append(w)
+            self.faces[face_id][3].append(h)
+        f = self.faces[face_id]
         return (int(np.mean(f[0])), int(np.mean(f[1])), int(np.mean(f[2])), int(np.mean(f[3])))
 
 
 def main(args):
     recognizer = cv2.face.LBPHFaceRecognizer_create()
-    recognizer.read('model/trainer.yml')   #load trained model
+    recognizer.read('model/trainer.yml')                # load trained model
     cascadePath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(cascadePath);
+    faceCascade = cv2.CascadeClassifier(cascadePath)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
-    #iniciate id counter, the number of persons you want to include
+    # iniciate id counter, the number of persons you want to include
 
     # Initialize and start realtime video capture
     cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    cam.set(3, 640) # set video widht
-    cam.set(4, 480) # set video height
+    cam.set(3, 640)    # set video widht
+    cam.set(4, 480)    # set video height
 
     # Define min window size to be recognized as a face
-    minW = 0.1*cam.get(3)
-    minH = 0.1*cam.get(4)
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
 
     profiles = Profiles()
 
@@ -63,20 +64,20 @@ def main(args):
 
     while True:
 
-        ret, img =cam.read()
+        ret, img = cam.read()
 
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        faces = faceCascade.detectMultiScale( 
+        faces = faceCascade.detectMultiScale(
             gray,
-            scaleFactor = 1.2,
-            minNeighbors = 5,
-            minSize = (int(minW), int(minH)),
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(int(minW), int(minH)),
         )
 
-        for(x,y,w,h) in faces:
-            id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-            # Check if confidence is less them 100 ==> "0" is perfect match 
+        for (x, y, w, h) in faces:
+            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+            # Check if confidence is less them 100 ==> "0" is perfect match
             if (confidence < 100):
                 found = False
                 for k, v in profiles.profiles.items():
@@ -89,16 +90,16 @@ def main(args):
             else:
                 id = "unknown"
                 confidence = "  {0}%".format(round(100 - confidence))
-            
+
             (x, y, w, h) = faces_avg.new_frame(x, y, w, h, id)
 
-            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-            cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-            cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-        
-        cv2.imshow('camera',img) 
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+            cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
-        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        cv2.imshow('camera', img)
+
+        k = cv2.waitKey(10) & 0xff     # Press 'ESC' for exiting video
         if k == 27:
             break
 
